2e65a4eb5390f948173f6bbb2b9afef718138183
Plato Leung <pleung@UNIHO45.localdomain>
2019-09-12 11:33:30 -0400

    learnt type double

--- MyClass.cs
+++ MyClass.cs
@@ -14,6 +14,13 @@ namespace myApp
             int max = int.MaxValue;
             int min = int.MinValue;
             Console.WriteLine($"The range of integers is {min} to {max}");
+
+            int what = max + 3;
+            Console.WriteLine($"An example of overflow: {what}");
+
+            double dmax = double.MaxValue;
+            double dmin = double.MinValue;
+            Console.WriteLine($"The range of double is {dmin} to {dmax}");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -20,6 +20,24 @@ namespace myApp
             Console.WriteLine(c);
         }
 
+        static void PlayWithDouble()
+        {
+            double a = 5;
+            double b = 4;
+            double c = 2;
+            double d = (a + b) / c;
+            Console.WriteLine(d);
+
+            double e = 19;
+            double f = 23;
+            double g = 8;
+            double h = (e  + f) / g;
+            Console.WriteLine(h);
+
+            double third = 1.0 / 3.0;
+            Console.WriteLine(third);
+        }
+
         static void MyFirstFunction()
         {
             Console.WriteLine("Hello World!");
@@ -34,6 +52,8 @@ namespace myApp
             Console.WriteLine($"Call MyClass ReturnMessage function from Program: {c1.ReturnMessage()}");
 
             MyClass.MinMax();
+
+            PlayWithDouble();
         }
     }
 }
