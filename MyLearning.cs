1bfe5e762e5ef20e46524d2bce0a63e30c5dc3eb
Plato Leung <platoleunghk@yahoo.com.hk>
2019-11-06 11:48:54 -0500

    add file via upload
    "mylearning.cs updated and tried throw expection

--- MyLearning.cs
+++ MyLearning.cs
@@ -2,19 +2,51 @@ using System;
 
 namespace myApp
 {
-    class MyLearning
+    /* class MyLearning
     {
         static void Main(string[] args)
         {
             int radius = 5;
             double result = CalculateArea(radius);
             Console.WriteLine("The area is {0:0.00}", result);
+
+
         }
 
         static double CalculateArea(int r)
         {
             double area = r * r * Math.PI;
             return area;
+        }
+    } */
+
+    public class NumberGenerator
+    {
+    int[] numbers = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+        
+        public int GetNumber(int index)
+        {
+            if (index < 0 || index >= numbers.Length) {
+                throw new IndexOutOfRangeException();
+            }
+            return numbers[index];
+        }
+    }
+
+    public class Example
+    {
+        public static void Main()
+        {
+            var gen = new NumberGenerator();
+            int index = 10;
+            try {
+                int value = gen.GetNumber(index);
+                Console.WriteLine($"Retrieved {value}");
+            }
+            catch (IndexOutOfRangeException e) 
+            {
+                Console.WriteLine($"{e.GetType().Name}: {index} is outside the bounds of the array");
+            }
         }
     }
 }
